fa3c6d7ee8711631d452e093f3af7f5108e90286
EvenSkje <31470603+EvenSkje@users.noreply.github.com>
2017-08-30 10:41:29 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * Byggern_prosjekt.c
+ *
+ * Created: 30.08.2017 10:35:32
+ *  Author: evenskj
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
